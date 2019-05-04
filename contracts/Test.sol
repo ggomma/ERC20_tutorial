ce534435e7ae80598cfb3456648bea2acd227349
ggomma <ggomma@gmail.com>
2019-05-04 06:27:09 +0000

    Implement AirDrop feature

--- contracts/Test.sol
+++ contracts/Test.sol
@@ -8,7 +8,25 @@ contract TEST is ERC20Pausable {
   uint public constant decimals = 18;
   uint public constant INITIAL_SUPPLY = 1000 * (10 ** decimals);
 
+  mapping (address => uint256) public airDropHistory;
+  event AirDrop(address _receiver, uint256 _amount);
+
   constructor() public {
     _mint(msg.sender, INITIAL_SUPPLY);
+  }
+
+  function dropToken(address[] memory receivers, uint256[] memory values) public {
+    require(receivers.length != 0);
+    require(receivers.length == values.length);
+
+    for (uint256 i = 0; i < receivers.length; i++) {
+      address receiver = receivers[i];
+      uint256 amount = values[i];
+
+      transfer(receiver, amount);
+      airDropHistory[receiver] += amount;
+
+      emit AirDrop(receiver, amount);
+    }
   }
 }
